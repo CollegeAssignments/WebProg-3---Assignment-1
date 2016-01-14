7e422599b5a9b4f25311f098553f0b742f1326d0
Fergal Gaffney <s00147036@mail.itsligo.ie>
2016-01-14 20:38:27 +0000

    Database changes

--- WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/mainDbEDM.Designer.cs
+++ WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/mainDbEDM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Programming\dotNet\WebProg-3---Assignment-1\WebProg 3 - Car Rental Website\WebProg 3 - Car Rental Website\mainDbEDM.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Fergal\Desktop\WebProg-3---Assignment-1\WebProg 3 - Car Rental Website\WebProg 3 - Car Rental Website\mainDbEDM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
