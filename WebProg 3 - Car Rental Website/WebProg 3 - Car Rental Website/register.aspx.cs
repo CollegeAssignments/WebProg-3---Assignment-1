36484e79d8d3ad8abf59c86c7996735a51789935
Brian Geraghty <brian_geraghty@outlook.com>
2016-01-14 17:39:00 +0000

    Misc changes

--- WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/register.aspx.cs
+++ WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/register.aspx.cs
@@ -31,6 +31,7 @@ namespace WebProg_3___Car_Rental_Website
             string password = tbxPassword.Text;
             string confirmPassword = tbxConfirmPassword.Text;
             string passHashed;
+
             //MD5 Class
             //https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5(v=vs.110).aspx
 
@@ -43,21 +44,12 @@ namespace WebProg_3___Car_Rental_Website
 
         static string GetMd5Hash(MD5 md5Hash, string input)
         {
-            // Convert the input string to a byte array and compute the hash.
             byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
             StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
             for (int i = 0; i < data.Length; i++)
             {
                 sBuilder.Append(data[i].ToString("x2"));
             }
-
-            // Return the hexadecimal string.
             return sBuilder.ToString();
         }
     }
