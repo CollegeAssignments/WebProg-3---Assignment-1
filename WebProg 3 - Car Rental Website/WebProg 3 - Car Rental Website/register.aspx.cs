d92f14e23c04e119690b41fefaba5c8a4c26d34d
Brian Geraghty <brian_geraghty@outlook.com>
2016-01-15 20:43:17 +0000

    Random salt generator working. Password hashing working

--- WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/register.aspx.cs
+++ WebProg 3 - Car Rental Website/WebProg 3 - Car Rental Website/register.aspx.cs
@@ -30,27 +30,69 @@ namespace WebProg_3___Car_Rental_Website
 
             string password = tbxPassword.Text;
             string confirmPassword = tbxConfirmPassword.Text;
-            string passHashed;
 
-            //MD5 Class
-            //https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5(v=vs.110).aspx
+            string salt = GenerateSalt();
+            string passHashed = HashMD5(password+salt);
 
-            using (MD5 md5Hash = MD5.Create())
-            {
-                passHashed = GetMd5Hash(md5Hash, password);
-            }
-            while (true) { }
         }
 
-        static string GetMd5Hash(MD5 md5Hash, string input)
+
+        //Generate a random alphnumeric salt
+        //Source: http://stackoverflow.com/questions/31957255/generating-a-random-number-in-c-sharp-with-alpha-numeric-values
+        public static string GenerateSalt(int size = 8, string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
         {
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-            StringBuilder sBuilder = new StringBuilder();
-            for (int i = 0; i < data.Length; i++)
+            using (var crypto = new RNGCryptoServiceProvider())
             {
-                sBuilder.Append(data[i].ToString("x2"));
+                var data = new byte[size];
+
+                // If chars.Length isn't a power of 2 then there is a bias if
+                // we simply use the modulus operator. The first characters of
+                // chars will be more probable than the last ones.
+
+                // buffer used if we encounter an unusable random byte. We will
+                // regenerate it in this buffer
+                byte[] smallBuffer = null;
+
+                // Maximum random number that can be used without introducing a
+                // bias
+                int maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % chars.Length);
+
+                crypto.GetBytes(data);
+
+                var result = new char[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    byte v = data[i];
+
+                    while (v > maxRandom)
+                    {
+                        if (smallBuffer == null)
+                        {
+                            smallBuffer = new byte[1];
+                        }
+
+                        crypto.GetBytes(smallBuffer);
+                        v = smallBuffer[0];
+                    }
+
+                    result[i] = chars[v % chars.Length];
+                }
+
+                return new string(result);
             }
-            return sBuilder.ToString();
+        }
+
+        //Generates MD5 hashed password
+        //
+        //Accepts one string [password + salt]
+        //Returns MD5 hash as a string
+        string HashMD5(string input)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(Encoding.Unicode.GetBytes(input));
+            string result = BitConverter.ToString(bytes).Replace("-", String.Empty);
+            return result;
         }
     }
 }
